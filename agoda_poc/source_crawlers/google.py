c671442a1141f4cdd2fe009f80d8c671064ecf55
Sajidur Rahman <sajidur.rahman@knolskape.com>
2020-02-06 15:18:57 +0530

    added support for partial download handling

--- agoda_poc/source_crawlers/google.py
+++ agoda_poc/source_crawlers/google.py
@@ -1,6 +1,7 @@
 import requests
 import os
 import re
+from urllib.parse import unquote
 from alive_progress import alive_bar
 import math
 
@@ -11,20 +12,62 @@ class GoogleDriveDownloader:
         if not os.path.isdir(self.destPathPrefix):
             os.makedirs(self.destPathPrefix)
 
+    def getFilename(self, s):
+        fname = re.findall("filename\*=([^;]+)", s, flags=re.IGNORECASE)
+        if not fname:
+            fname = re.findall("filename=([^;]+)", s, flags=re.IGNORECASE)
+        if "utf-8''" in fname[0].lower():
+            fname = re.sub("utf-8''", '', fname[0], flags=re.IGNORECASE)
+            fname = unquote(fname)
+        else:
+            fname = fname[0]
+        # clean space and double quotes
+        return fname.strip().strip('"')
+
     def downloadFileInChunks(self, downloadURL):
-        with requests.get(downloadURL, stream=True) as r:
-            r.raise_for_status()
-            total_size = int(r.headers.get('content-length', 0))
-            block_size = 8192
-        
-            with open(self.destPathPrefix+'/tempFile', 'wb') as f:
-                with alive_bar(math.ceil(total_size/block_size)) as bar:
+        r = requests.get(downloadURL, stream=True)
+        r.raise_for_status()
+        total_size = int(r.headers.get('content-length', 0))
+        block_size = 8192
+    
+        with open(self.destPathPrefix+'/tempFile', 'wb') as f:
+            with alive_bar(math.ceil(total_size/block_size)) as bar:
+                try:
                     for chunk in r.iter_content(chunk_size=block_size): 
                         if chunk: # filter out keep-alive new chunks
                             f.write(chunk)
                             bar()
+                except Exception as e:
+                    if os.path.exists(f"{self.destPathPrefix}/tempFile"):
+                        os.remove(f"{self.destPathPrefix}/tempFile")
+                        return None
         cd = r.headers['content-disposition']
-        fname = re.findall("filename=(.+)", cd)[0].split(';')[0].replace('\"','')
+        fname = self.getFilename(cd)
+        os.rename(f"{self.destPathPrefix}/tempFile", f"{self.destPathPrefix}/{fname}")
+        r.close()
+        return fname
+
+
+    def downloadFileInChunksPartialStream(self, downloadURL):
+        r = requests.get(downloadURL, stream=True)
+        r.raise_for_status()
+        total_size = int(r.headers.get('content-length', 0))
+        block_size = 8192
+    
+        with open(self.destPathPrefix+'/tempFile', 'wb') as f:
+            with alive_bar(math.ceil(total_size/block_size)) as bar:
+                try:
+                    for chunk in r.iter_content(chunk_size=block_size): 
+                        if chunk: # filter out keep-alive new chunks
+                            f.write(chunk)
+                            bar()
+                            break
+                except Exception as e:
+                    if os.path.exists(f"{self.destPathPrefix}/tempFile"):
+                        os.remove(f"{self.destPathPrefix}/tempFile")
+                        return None
+        cd = r.headers['content-disposition']
+        fname = self.getFilename(cd)
         os.rename(f"{self.destPathPrefix}/tempFile", f"{self.destPathPrefix}/{fname}")
         r.close()
         return fname
@@ -32,4 +75,9 @@ class GoogleDriveDownloader:
     def download(self):
         downloadURL = f'https://drive.google.com/uc?id={self.fileId}'
         fname = self.downloadFileInChunks(downloadURL)
+        return fname, True
+
+    def downloadPartialStream(self):
+        downloadURL = f'https://drive.google.com/uc?id={self.fileId}'
+        fname = self.downloadFileInChunksPartialStream(downloadURL)
         return fname, True
--- agoda_poc/source_crawlers/oneDrive.py
+++ agoda_poc/source_crawlers/oneDrive.py
@@ -15,25 +15,69 @@ class OneDriveDownloader:
         if not os.path.isdir(self.destPathPrefix):
             os.makedirs(self.destPathPrefix)
 
+    def getFilename(self, s):
+        fname = re.findall("filename\*=([^;]+)", s, flags=re.IGNORECASE)
+        if not fname:
+            fname = re.findall("filename=([^;]+)", s, flags=re.IGNORECASE)
+        if "utf-8''" in fname[0].lower():
+            fname = re.sub("utf-8''", '', fname[0], flags=re.IGNORECASE)
+            fname = unquote(fname)
+        else:
+            fname = fname[0]
+        # clean space and double quotes
+        return fname.strip().strip('"')
+
     def getEncodedSharingUrl(self):
         encoded = self.shareLink.encode()
         return 'u!'+b64e(encoded).decode().rstrip('=').replace('/','_').replace('+','-')
 
     def downloadFileInChunks(self, downloadUrl):
-        with requests.get(downloadUrl, stream=True) as r:
-            r.raise_for_status()
-            total_size = int(r.headers.get('content-length', 0))
-            block_size = 8192
+        r = requests.get(downloadUrl, stream=True)
+        r.raise_for_status()
+        total_size = int(r.headers.get('content-length', 0))
+        block_size = 8192
 
-            with open(self.destPathPrefix+'/tempFile', 'wb') as f:
-                with alive_bar(math.ceil(total_size/block_size)) as bar:
+        with open(self.destPathPrefix+'/tempFile', 'wb') as f:
+            with alive_bar(math.ceil(total_size/block_size)) as bar:
+                try:
                     for chunk in r.iter_content(chunk_size=8192): 
                         if chunk: # filter out keep-alive new chunks
                             f.write(chunk)
                             bar()
+                except Exception as e:
+                    if os.path.exists(f"{self.destPathPrefix}/tempFile"):
+                        os.remove(f"{self.destPathPrefix}/tempFile")
+                        return None
+        
         cd = r.headers['content-disposition']
-        fname = re.findall("filename(.+)", cd)[0].replace('*=UTF-8\'\'','')
-        fname = unquote(fname)
+        fname=self.getFilename(cd)
+        print(fname)
+        os.rename(f"{self.destPathPrefix}/tempFile", f"{self.destPathPrefix}/{fname}")
+        r.close()
+        return fname
+
+
+    def downloadFileInChunksPartialStream(self, downloadUrl):
+        r = requests.get(downloadUrl, stream=True)
+        r.raise_for_status()
+        total_size = int(r.headers.get('content-length', 0))
+        block_size = 8192
+
+        with open(self.destPathPrefix+'/tempFile', 'wb') as f:
+            with alive_bar(math.ceil(total_size/block_size)) as bar:
+                try:
+                    for chunk in r.iter_content(chunk_size=8192): 
+                        if chunk: # filter out keep-alive new chunks
+                            f.write(chunk)
+                            bar()
+                            break
+                except Exception as e:
+                    if os.path.exists(f"{self.destPathPrefix}/tempFile"):
+                        os.remove(f"{self.destPathPrefix}/tempFile")
+                        return None
+        
+        cd = r.headers['content-disposition']
+        fname=self.getFilename(cd)
         print(fname)
         os.rename(f"{self.destPathPrefix}/tempFile", f"{self.destPathPrefix}/{fname}")
         r.close()
@@ -43,4 +87,10 @@ class OneDriveDownloader:
         encodedSharingUrl = self.getEncodedSharingUrl()
         downloadUrl = f"https://api.onedrive.com/v1.0/shares/{encodedSharingUrl}/root/content"
         fname = self.downloadFileInChunks(downloadUrl)
+        return fname, True
+
+    def downloadPartialStream(self):
+        encodedSharingUrl = self.getEncodedSharingUrl()
+        downloadUrl = f"https://api.onedrive.com/v1.0/shares/{encodedSharingUrl}/root/content"
+        fname = self.downloadFileInChunksPartialStream(downloadUrl)
         return fname, True
